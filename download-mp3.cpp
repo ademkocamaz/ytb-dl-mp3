388380ccc54b72935e634a93a392014a0e41b2be
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-04-23 23:14:14 +0300

    Update download-mp3.cpp

--- download-mp3.cpp
+++ download-mp3.cpp
@@ -2,6 +2,6 @@
 
 int main(int argc, char** argv) {
 	system("youtube-dl.exe --verbose --extract-audio --audio-format mp3 --batch-file url.txt");
-	system("move /y *.mp3 Download-mp3");
+	//system("move /y *.mp3 Download-mp3");
 
 }
